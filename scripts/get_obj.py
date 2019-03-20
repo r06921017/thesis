84d6bb1f45be58a25e75542f1a7b68d932078933
r06921017 <r06921017@ntu.edu.tw>
2019-03-20 11:42:50 +0800

    Create a ros package for my thesis

---
+++ scripts/get_obj.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+"""
+This is for object detection on Pepper
+"""
+
+import rospy
+from darknet_ros_msgs.msg import *
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from robot_project.msg import ObjPoseArray, ObjPose
+import tf
+from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import PoseStamped, Point
+from visualization_msgs.msg import Marker
+from std_msgs.msg import ColorRGBA
+
+
+class VideoFrames:
+    """
+    Reference : ros-video-recorder
+    https://github.com/ildoonet/ros-video-recorder/blob/master/scripts/recorder.py
+    """
+
+    def __init__(self, image_topic):
+        self.image_sub = rospy.Subscriber(image_topic, Image, self.callback_image, queue_size=1)
+        self.bridge = CvBridge()
+        self.frames = []
+
+    def callback_image(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data)
+
+        except CvBridgeError as e:
+            rospy.logerr('Converting Image Error. ' + str(e))
+            return
+
+        self.frames.append((data.header.stamp, cv_image))
+
+    def get_latest(self, at_time, remove_older=True):
+        fs = [x for x in self.frames if x[0] <= at_time]
+        if len(fs) == 0:
+            return None
+
+        f = fs[-1]
+        if remove_older:
+            self.frames = self.frames[len(fs) - 1:]
+
+        return f[1]
+
+
+def camera2pose(depth, pixel_x, pixel_y, target_frame):
+
+    # x, y, z direction are relative to robot frame (unit: meters)
+    obj_depth_frame = PoseStamped()
+    obj_depth_frame.header.frame_id = "CameraDepth_frame"
+    obj_depth_frame.pose.position.x = np.round(depth / 1000, 4)
+    obj_depth_frame.pose.position.y = np.round(((pixel_x - cx) * depth / fx) / 1000, 4)
+    obj_depth_frame.pose.position.z = np.round(((cy - pixel_y) * depth / fy) / 1000, 4)
+    obj_depth_frame.pose.orientation.w = 1.0  # Neural orientation
+
+    if target_frame != "CameraDepth_frame":
+        listener = tf.TransformListener()
+        obj_target_frame = listener.transformPose(target_frame, obj_depth_frame)  # convert into /map frame
+
+    else:
+        obj_target_frame = obj_depth_frame
+
+    return obj_target_frame.pose.position.x, obj_target_frame.pose.position.y
+
+
+def draw_map(pose_x, pose_y, class_name, temp_map, robot_pose_offset):
+    pose_string = '(' + str(pose_x) + ', ' + str(pose_y) + ')'  # pose_x, pose_y unit:m
+    pixel_per_length = 1  # unit:1 cm = 1 pixels
+    temp_x = temp_map.shape[1] // 2 + int(pose_y * 100) * pixel_per_length
+    temp_y = (temp_map.shape[0] - robot_pose_offset) - int(pose_x * 100) * pixel_per_length
+
+    cv2.circle(temp_map, (temp_x, temp_y), 3, (255, 0, 0), 2)
+    cv2.putText(temp_map, class_name, (temp_x + 5, temp_y + 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    cv2.putText(temp_map, pose_string, (temp_x + 5, temp_y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+
+    return
+
+
+def create_ros_marker(input_obj_array, marker_scale=0.5, red=1.0, green=1.0, blue=0.0, alpha=1.0):  # input point list
+
+    pos = Marker()  # type: Marker
+    pos.header.frame_id = 'map'
+    pos.header.stamp = rospy.Time.now()
+    pos.ns = 'obj_pos'
+    pos.type = 8  # point
+    pos.action = 0  # Add
+
+    pos.pose.position.x = 0.0
+    pos.pose.position.y = 0.0
+
+    pos.pose.orientation.x = 0.0
+    pos.pose.orientation.y = 0.0
+    pos.pose.orientation.z = 0.0
+    pos.pose.orientation.w = 1.0
+
+    pos.scale.x = marker_scale
+    pos.scale.y = marker_scale
+    pos.scale.z = marker_scale
+
+    for ele in input_obj_array:
+        p = Point()
+        p.x = ele.x
+        p.y = ele.y
+        p.z = 0.05
+
+        c = ColorRGBA()
+        c.r = red
+        c.g = green
+        c.b = blue
+        c.a = alpha
+
+        pos.points.append(p)
+        pos.colors.append(c)
+
+    return pos
+
+
+def bd_callback(data):
+    global obj_pose_array, obj_buffer
+
+    # get image with pose time
+    t = data.header.stamp
+    depth_img = df.get_latest(t, remove_older=True)
+
+    if depth_img is None:
+        rospy.logwarn('No received depth images.')
+        return
+
+    temp_map_width = 512
+    robot_pose_offset = 10  # pixel
+    temp_map = np.ones((temp_map_width, temp_map_width, 3), np.uint8) * 255  # show object location relative to robot
+
+    # Draw a robot
+    cv2.circle(temp_map, (temp_map_width // 2, temp_map_width - robot_pose_offset), 3, (0, 0, 255), 2)
+    cv2.putText(temp_map, 'robot', (temp_map_width // 2 + 5, temp_map_width - robot_pose_offset),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    obj_list = []
+    glob_obj_list = []
+
+    for obj in data.bounding_boxes:
+
+        if obj.Class != 'person':
+            bound_depth_img = np.copy(depth_img[obj.ymin:obj.ymax, obj.xmin:obj.xmax])
+            bound_depth_img[bound_depth_img > 5000] = 5000  # max range = 5m
+
+            # slide_width = np.min([bound_depth_img.shape[0], bound_depth_img.shape[1]]) // 4  # for object
+            slide_width = 30  # for human detection
+
+            sxmin = bound_depth_img.shape[1] // 2 - slide_width // 2
+            sxmax = bound_depth_img.shape[1] // 2 + slide_width // 2
+            symin = bound_depth_img.shape[0] // 2 - slide_width // 2
+            symax = bound_depth_img.shape[0] // 2 + slide_width // 2
+
+            search_box = bound_depth_img[symin:symax, sxmin:sxmax]
+            depth_val = np.mean(search_box)
+
+            obj_pixel_x = obj.xmin + bound_depth_img.shape[1] // 2
+            obj_pixel_y = obj.ymin + bound_depth_img.shape[0] // 2
+
+            # Draw on depth image
+            depth_img = cv2.circle(depth_img, (obj.xmin + bound_depth_img.shape[1] // 2,
+                                               obj.ymin + bound_depth_img.shape[0] // 2), 3, 100, 2)
+            depth_img = cv2.rectangle(depth_img, (obj.xmin + sxmin, obj.ymin + symin),
+                                      (obj.xmin + sxmax, obj.ymin + symax), 200, 2)
+
+            depth_img = cv2.rectangle(depth_img, (obj.xmin, obj.ymin), (obj.xmax, obj.ymax), 255, 2)
+
+            # Convert camera data to pose
+            obj_x, obj_y = camera2pose(depth_val, obj_pixel_x, obj_pixel_y, "CameraDepth_frame")  # unit: meter
+
+            temp_obj = ObjPose()  # type: ObjPose
+            temp_obj.Class = obj.Class
+            temp_obj.x = obj_x
+            temp_obj.y = obj_y
+
+            if obj_buffer.Class == '' and obj_buffer.x == 0.0 and obj_buffer.y == 0.0:  # empty buffer
+                obj_buffer = temp_obj
+
+            else:
+                perturbation = np.round(
+                    np.sqrt((obj_buffer.x - temp_obj.x) ** 2.0 + (obj_buffer.y - temp_obj.y) ** 2.0), 2)
+
+                if perturbation < (0.22 * temp_obj.x + 0.01 * temp_obj.y):  # unit: meter
+                    obj_buffer = temp_obj
+
+            # draw_map(temp_obj.x, temp_obj.y, temp_obj.Class, temp_map, robot_pose_offset)
+            draw_map(obj_buffer.x, obj_buffer.y, obj_buffer.Class, temp_map, robot_pose_offset)
+            obj_list.append(temp_obj)  # record all the object, not for single human detection
+
+            # Convert points to map frame
+            glob_obj_pose = ObjPose()
+            glob_obj_pose.Class = obj.Class
+            glob_obj_pose.x, glob_obj_pose.y = camera2pose(depth_val, obj_pixel_x, obj_pixel_y, "map")  # unit: meter
+            glob_obj_list.append(glob_obj_pose)
+
+    # Publish obj_list
+    obj_pose_array = obj_list
+    obj_pose_pub.publish(obj_pose_array)
+
+    obj_marker = create_ros_marker(glob_obj_list)
+    obj_mark_pub.publish(obj_marker)
+
+    depth_box_pub.publish(cv_bridge.cv2_to_imgmsg(depth_img))
+    temp_map_pub.publish(cv_bridge.cv2_to_imgmsg(temp_map, 'bgr8'))
+
+    return
+
+
+def get_cam_info(data):  # camera_info
+    global fx, fy, cx, cy, cam_width, cam_height
+
+    cam_width, cam_height = data.width, data.height
+    fx, cx, fy, cy = data.K[0], data.K[2], data.K[4], data.K[5]
+    rospy.loginfo('get camera info done!')
+    cam_info_sub.unregister()  # subscribe only once
+
+    return
+
+
+if __name__ == '__main__':
+    rospy.init_node('get_obj', anonymous=True)
+    rospy.loginfo('get_obj initialization')
+
+    # initialization (global variables)
+    depth_topic = '/naoqi_driver_node/camera/depth/image_raw'  # for pepper
+    camera_info_topic = '/naoqi_driver_node/camera/front/camera_info'  # for pepper
+    # depth_topic = '/camera/depth_registered/image_raw'  # for kinetic v1
+    # camera_info_topic = '/camera/rgb/camera_info'  # for kinetic v1
+
+    # Camera info subscribe once
+    fx, fy, cx, cy, cam_width, cam_height = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    cam_info_sub = rospy.Subscriber(camera_info_topic, CameraInfo, get_cam_info)
+    rospy.loginfo('fx, fy, cx, cy = %.4f, %.4f, %.4f, %.4f' % (fx, fy, cx, cy))
+
+    obj_pose_array = ObjPoseArray()
+    cv_bridge = CvBridge()
+    df = VideoFrames(depth_topic)  # get depth image
+
+    obj_buffer = ObjPose()  # for single object(human)
+
+    rospy.loginfo('Wait for depth_topic')
+    rospy.wait_for_message(depth_topic, Image, timeout=30)
+
+    # Publisher
+    depth_box_pub = rospy.Publisher("/get_obj/depth_box", Image, queue_size=1)
+    temp_map_pub = rospy.Publisher("/get_obj/temp_map", Image, queue_size=1)
+    obj_pose_pub = rospy.Publisher("/get_obj/obj_pose", ObjPoseArray, queue_size=10)
+    obj_mark_pub = rospy.Publisher('/get_obj/obj_marker', Marker, queue_size=1)
+
+    rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, bd_callback, queue_size=1)
+
+    rate = rospy.Rate(10)  # unit: hz
+
+    rospy.loginfo('Start finding object!')
+
+    while not rospy.is_shutdown():
+        try:
+            rate.sleep()
+            rospy.spin()
+
+        except rospy.ROSInterruptException:
+            rospy.loginfo('Shut down get objects ...')
---
+++ scripts/pepper_openpose.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Turn on tf-openpose with Pepper and visualize with ros imgmsg
+Input: images from both top and bottom camera from Pepper
+"""
+
+import os
+import sys
+import numpy as np
+import cv2
+import time
+
+from threading import Lock
+import rospy
+import rospkg
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from tfpose_ros.msg import Persons, Person, BodyPartElm
+
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import model_wh, get_graph_path
+
+
+def humans_to_msg(humans):
+    persons = Persons()
+
+    for human in humans:
+        person = Person()
+
+        for k in human.body_parts:
+            body_part = human.body_parts[k]
+
+            body_part_msg = BodyPartElm()
+            body_part_msg.part_id = body_part.part_idx
+            body_part_msg.x = body_part.x
+            body_part_msg.y = body_part.y
+            body_part_msg.confidence = body_part.score
+            person.body_part.append(body_part_msg)
+        persons.persons.append(person)
+
+    return persons
+
+
+def callback_image(data):
+    fps_time = time.time()
+    bottom_msg = rospy.wait_for_message('/naoqi_driver_node/camera/bottom/image_raw', Image)
+
+    try:
+        cv_front = cv_bridge.imgmsg_to_cv2(data, "bgr8")
+        cv_bottom = cv_bridge.imgmsg_to_cv2(bottom_msg, "bgr8")
+        cv_image = np.append(cv_front, cv_bottom, axis=0)
+
+    except CvBridgeError as err:
+        rospy.logerr('[tf-pose-estimation] Converting Image Error. ' + str(err))
+        return
+
+    acquired = tf_lock.acquire(False)
+    if not acquired:
+        return
+
+    try:
+        humans = pose_estimator.inference(cv_image, resize_to_default=True, upsample_size=resize_out_ratio)
+
+        # draw
+        pose_image = TfPoseEstimator.draw_humans(cv_image, humans, imgcopy=False)
+        cv2.putText(pose_image, "FPS: %f" % (1.0 / (time.time() - fps_time)),
+                    (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (0, 255, 0), 2)
+        pub_img.publish(cv_bridge.cv2_to_imgmsg(pose_image, 'bgr8'))
+
+    finally:
+        tf_lock.release()
+
+    msg = humans_to_msg(humans)
+    msg.image_w = data.width
+    msg.image_h = data.height
+    msg.header = data.header
+
+    pub_pose.publish(msg)
+
+
+if __name__ == '__main__':
+    rospy.loginfo('initialization+')
+    rospy.init_node('TfPoseEstimatorROS', anonymous=True, log_level=rospy.INFO)
+
+    # parameters
+    image_topic = rospy.get_param('~camera', '/naoqi_driver_node/camera/front/image_raw')
+    model = rospy.get_param('~model', 'mobilenet_thin')  # cmu
+
+    resolution = rospy.get_param('~resolution', '320x240')  # w x h
+    resize_out_ratio = float(rospy.get_param('~resize_out_ratio', '4.0'))
+    tf_lock = Lock()
+
+    if not image_topic:
+        rospy.logerr('Parameter \'camera\' is not provided.')
+        sys.exit(-1)
+
+    try:
+        w, h = model_wh(resolution)
+        graph_path = get_graph_path(model)
+
+        rospack = rospkg.RosPack()
+        graph_path = os.path.join(rospack.get_path('tfpose_ros'), graph_path)
+    except Exception as e:
+        rospy.logerr('invalid model: %s, e=%s' % (model, e))
+        sys.exit(-1)
+
+    path = graph_path
+    pose_estimator = TfPoseEstimator(path, target_size=(w, h))
+    cv_bridge = CvBridge()
+
+    rospy.Subscriber(image_topic, Image, callback_image, queue_size=1, buff_size=2**24)
+    pub_pose = rospy.Publisher('/robot_project/human_pose', Persons, queue_size=1)
+    pub_img = rospy.Publisher('/robot_project/pose_visualization', Image, queue_size=1)
+
+    rospy.loginfo('start+')
+    rospy.spin()
+    rospy.loginfo('finished')
