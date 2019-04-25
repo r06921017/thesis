67522e2f811fb916d179f0d6c979acabc6555691
r06921017 <r06921017@ntu.edu.tw>
2019-04-25 11:51:56 +0800

    Add networkx for visualization.

--- scripts/networkx_try.py
+++ scripts/networkx_try.py
@@ -3,6 +3,7 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
     # G = nx.Graph()
@@ -20,55 +21,82 @@ if __name__ == '__main__':
     loc_symbol_val = loc_symbol.values()
 
     G = nx.Graph()
-
-    G.add_node(0, pos=(0, 5))
-    G.add_node(1, pos=(2, 5))
-    G.add_node(2, pos=(2, 4))
-    G.add_node(3, pos=(1, 1))
+    G.add_node(0, pos=(0, 9))
+    G.add_node(1, pos=(12, 9))
+    G.add_node(2, pos=(12, 7))
+    G.add_node(3, pos=(8, 1))
     G.add_node(4, pos=(0, 1))
-    G.add_node(5, pos=(2, 1))
+    G.add_node(5, pos=(12, 1))
     G.add_node(6, pos=(0, 0))
-    G.add_node(7, pos=(1, 0))
+    G.add_node(7, pos=(8, 0))
+    G.add_node(8, pos=(8, 7))
 
     G.add_edge(0, 1, weight=12)
-    G.add_edge(0, 2, weight=13)
-    G.add_edge(0, 3, weight=14)
-    G.add_edge(0, 4, weight=15)
+    G.add_edge(0, 8, weight=9)
+    G.add_edge(8, 3, weight=6)
+    G.add_edge(8, 2, weight=4)
     G.add_edge(1, 2, weight=2)
-    G.add_edge(2, 3, weight=10)
     G.add_edge(3, 4, weight=7)
     G.add_edge(3, 5, weight=4)
     G.add_edge(3, 7, weight=1)
     G.add_edge(4, 6, weight=1)
     G.add_edge(6, 7, weight=8)
 
-    pos = nx.get_node_attributes(G, 'pos')
-    nx.draw(G, pos, with_labels=True, node_size=500, node_color='yellow')
-    labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-    plt.savefig("/home/rdaneel/temp_123.png")
-
-    # print G.edges
-    #
-    # for e in G.edges.values():
-    #     print e['weight']
-
     G1 = nx.Graph()
-
-    G1.add_node(0, pos=(0, 5))
-    G1.add_node(1, pos=(2, 5))
-    G1.add_node(2, pos=(2, 4))
-    G1.add_node(3, pos=(1, 1))
-    G1.add_node(4, pos=(0, 1))
-    G1.add_node(5, pos=(2, 1))
-    G1.add_node(6, pos=(0, 0))
-    G1.add_node(7, pos=(1, 0))
+    G1.add_node('0', pos=(0, 9))
+    G1.add_node('1', pos=(12, 9))
+    G1.add_node('2', pos=(12, 7))
+    G1.add_node('3', pos=(8, 1))
+    G1.add_node('4', pos=(0, 1))
+    G1.add_node('5', pos=(12, 1))
+    G1.add_node('6', pos=(0, 0))
+    G1.add_node('7', pos=(8, 0))
+    G1.add_node('8', pos=(8, 7))
 
     for e in G.edges.items():
-        node1 = e[0][0]
-        node2 = e[0][1]
+        node1 = str(e[0][0])
+        node2 = str(e[0][1])
         weight = e[1]['weight']
 
-        # print e
-        # print 'node = {0}, {1}'.format(node1, node2)
-        # print 'w = ', weight
+        print 'node = {0}, {1}'.format(node1, node2)
+        print 'w = ', weight
+
+        previous_node = node1
+
+        start_node = G.node[e[0][0]]['pos']
+        end_node = G.node[e[0][1]]['pos']
+
+        u_vec = tuple(np.subtract(end_node, start_node) / float(weight))
+
+        print 'unit vector = ', u_vec
+
+        for i in range(1, weight):
+            print 'add = ', np.add(start_node, tuple(np.multiply(u_vec, i)))
+            new_pos = tuple(np.add(start_node, tuple(np.multiply(u_vec, i))))
+            print 'new_pos = ', new_pos
+            temp_node = node1 + node2 + '_' + str(i)
+            G1.add_node(temp_node, pos=new_pos)
+            G1.add_edge(previous_node, temp_node)
+            previous_node = temp_node
+
+        G1.add_edge(previous_node, node2)
+
+    pos = nx.get_node_attributes(G1, 'pos')
+    nx.draw(G1, pos, with_labels=False, node_size=100, node_color='brown')
+    # labels = nx.get_edge_attributes(G1, 'weight')
+    # nx.draw_networkx_edge_labels(G1, pos, edge_labels=labels)
+
+    G2 = nx.Graph()
+    G2.add_node('0', pos=(0, 9))
+    G2.add_node('1', pos=(12, 9))
+    G2.add_node('2', pos=(12, 7))
+    G2.add_node('3', pos=(8, 1))
+    G2.add_node('4', pos=(0, 1))
+    G2.add_node('5', pos=(12, 1))
+    G2.add_node('6', pos=(0, 0))
+    G2.add_node('7', pos=(8, 0))
+
+    pos = nx.get_node_attributes(G2, 'pos')
+    nx.draw(G2, pos, with_labels=True, node_size=500, node_color='orange')
+
+    plt.savefig("/home/rdaneel/temp_graph.png")
